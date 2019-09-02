d2d7e15593d9b6cf9db4c50c7ab7510a6484170d
popdo <bme@qq.com>
2019-09-02 16:01:42 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','微博')-微博</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
- <div class="content">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+    </div>
+</nav>
+ <div class="container">
      @yield('content')
 </div>
 @yield('script')
