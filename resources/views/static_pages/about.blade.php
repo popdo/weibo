2c74ed9d31ab3a30a991f601a86c15fcb6a7cacb
popdo <bme@qq.com>
2019-09-02 15:38:24 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+<div class="container">
+    <h1>关于</h1>
+</div>
+@endsection
+
+@section('script')
+
+@endsection
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+@section('title','首页')
+@section('content')
+<div class="container">
+    <h1>首页</h1> 
+</div>
+@endsection
+
+@section('script')
+
+@endsection
