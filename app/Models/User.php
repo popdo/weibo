0390ba9e185918275e7f6424f990bdaf0929b65f
popdo <bme@qq.com>
2019-09-02 16:54:22 +0000

    user模型移动到model文件夹

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notifiable; // 消息通知相关功能引
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -21,7 +21,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏，则可使用 hidden 属性：
      * @var array
      */
     protected $hidden = [
